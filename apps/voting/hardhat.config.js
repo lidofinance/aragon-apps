4d73368514b3ee49a5827d3916c5e26289b48085
Eugene Mamin <TheDZhon@gmail.com>
2022-03-31 12:40:16 +0000

    feat: use hardhat tooling for voting app
    Remove outdated buidler.

---
+++ apps/voting/hardhat.config.js
@@ -0,0 +1,9 @@
+require('@nomiclabs/hardhat-web3')
+require('@nomiclabs/hardhat-ethers')
+require('@nomiclabs/hardhat-truffle5')
+require('@nomiclabs/hardhat-ganache')
+require('@nomiclabs/hardhat-etherscan')
+require('hardhat-gas-reporter')
+require('solidity-coverage')
+
+module.exports = require('@aragon/hardhat-config')
