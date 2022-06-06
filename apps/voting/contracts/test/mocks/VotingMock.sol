980571d87e3275b9b2a7a2cb24b5012d380ede86
Eugene Mamin <TheDZhon@gmail.com>
2022-06-06 10:10:39 +0000

    test: remove newVoteExt, improve test coverage

--- apps/voting/contracts/test/mocks/VotingMock.sol
+++ apps/voting/contracts/test/mocks/VotingMock.sol
@@ -5,18 +5,6 @@ import "@aragon/contract-helpers-test/contracts/0.4/aragonOS/TimeHelpersMock.sol
 
 
 contract VotingMock is Voting, TimeHelpersMock {
-    /* Ugly hack to work around this issue:
-     * https://github.com/trufflesuite/truffle/issues/569
-     * https://github.com/trufflesuite/truffle/issues/737
-     */
-    function newVoteExt(bytes _executionScript, string _metadata, bool _castVote, bool _executesIfDecided)
-        external
-        returns (uint256 voteId)
-    {
-        voteId = _newVote(_executionScript, _metadata, _castVote);
-        emit StartVote(voteId, msg.sender, _metadata);
-    }
-
     // _isValuePct public wrapper
     function isValuePct(uint256 _value, uint256 _total, uint256 _pct) external pure returns (bool) {
         return _isValuePct(_value, _total, _pct);
