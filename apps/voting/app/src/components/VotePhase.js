3ea5c55d38b5aada1f65b871b0209f843088e9b9
Azat Serikov <azatsdev@gmail.com>
2022-06-20 10:38:38 +0600

    fix: use text for phases

--- apps/voting/app/src/components/VotePhase.js
+++ apps/voting/app/src/components/VotePhase.js
@@ -2,60 +2,53 @@ import React from 'react'
 import { GU, textStyle, Timer, useTheme } from '@aragon/ui'
 import styled from 'styled-components'
 import { useVotePhase } from '../hooks/useVotePhase'
-import { PhaseIcon } from './PhaseIcon'
 
 const VotePhase = ({ vote }) => {
   const theme = useTheme()
   const { isMainPhase, isObjectionPhase } = useVotePhase(vote)
 
-  let phaseName = 'Voting over'
   if (isMainPhase) {
-    phaseName = 'Main phase'
-  } else if (isObjectionPhase) {
-    phaseName = 'Objection phase'
+    return (
+      <div
+        css={`
+          ${textStyle('body2')};
+          color: ${theme.contentSecondary};
+        `}
+      >
+        <PhaseLabel>Main phase</PhaseLabel>
+        <Timer end={vote.data.objectionPhaseStartDate} maxUnits={4} />
+      </div>
+    )
+  }
+
+  if (isObjectionPhase) {
+    return (
+      <div
+        css={`
+          ${textStyle('body2')};
+          color: ${theme.contentSecondary};
+        `}
+      >
+        <PhaseLabel>Objections phase</PhaseLabel>
+        <Timer end={vote.data.endDate} maxUnits={4} />
+      </div>
+    )
   }
 
   return (
     <div
       css={`
         ${textStyle('body2')};
+        color: ${theme.contentSecondary};
       `}
     >
-      <PhaseContainer>
-        <PhaseIcon vote={vote} />
-        <div
-          css={`
-            color: ${theme.contentSecondary};
-          `}
-        >
-          {phaseName}
-        </div>
-      </PhaseContainer>
-      {isMainPhase && (
-        <div
-          css={`
-            margin-top: ${2 * GU}px;
-          `}
-        >
-          <div
-            css={`
-              ${textStyle('body2')};
-              color: ${theme.surfaceContentSecondary};
-              margin-bottom: ${1 * GU}px;
-            `}
-          >
-            Objection phase in
-          </div>
-          <Timer end={vote.data.objectionPhaseStartDate} maxUnits={4} />
-        </div>
-      )}
+      <p>Voting over</p>
     </div>
   )
 }
 
-const PhaseContainer = styled.span`
-  display: flex;
-  align-items: center;
+const PhaseLabel = styled.p`
+  margin-bottom: ${GU}px;
 `
 
 export default VotePhase
