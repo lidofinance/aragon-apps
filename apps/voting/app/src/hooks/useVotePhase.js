0323ecc4e48108aeefe40e7ed6dc75eda88779b4
Azat Serikov <azatsdev@gmail.com>
2022-06-20 11:28:31 +0600

    fix: fix voteable states

--- apps/voting/app/src/hooks/useVotePhase.js
+++ apps/voting/app/src/hooks/useVotePhase.js
@@ -4,21 +4,19 @@ import useNow from './useNow'
 export const useVotePhase = vote => {
   const now = useNow()
 
-  const { open, objectionPhaseStartDate, endDate, executed } = vote.data
+  const { open, objectionPhaseStartDate, endDate } = vote.data
 
   const isMainPhase = open && isBefore(now, objectionPhaseStartDate)
   const isObjectionPhase =
     isAfter(now, objectionPhaseStartDate) && isBefore(now, endDate)
-  const voteOver = !open || (open && !executed)
 
-  const canVoteYes = open && !isObjectionPhase
-  const canVoteNo = open
+  const canVoteYes = isMainPhase
+  const canVoteNo = isMainPhase || isObjectionPhase
 
   return {
     isMainPhase,
     isObjectionPhase,
     canVoteYes,
     canVoteNo,
-    voteOver,
   }
 }
